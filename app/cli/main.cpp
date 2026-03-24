793ee94de13d959aa9afc46a4e4772bd9c956a67
wzx <673765578@qq.com>
2026-03-25 01:26:47 +0800

    fix:[]处理编译问题

--- app/cli/main.cpp
+++ app/cli/main.cpp
@@ -279,7 +279,7 @@ int main(int argc, char *argv[])
         if (line.isNull())
             break;
 
-        repl.processInput(line);
+        (void)repl.processInput(line);
     }
 
     return 0;
